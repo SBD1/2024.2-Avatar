00bece6c1da5b12420ffe8fe481e5fc46590c3d6
luciano-freitas-melo <202016847@aluno.unb.br>
2025-02-03 20:48:12 -0300

    refactor: ajusta trigger de combate

--- sql/t_combate.sql
+++ sql/t_combate.sql
@@ -1,13 +1,24 @@
 -- Quando um inimigo derrotado é derrotado o player ganha xp e moedas
-CREATE OR REPLACE FUNCTION inimigo_derrotado(id_pc INT, id_inimigo INT)
-RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION inimigo_derrotado()
+RETURNS TRIGGER AS $$
+DECLARE
+  p_xp_ganho INT;
+  p_num_moedas_ganho INT;
 BEGIN
-    UPDATE pc SET xp = xp + xp_ganho, moedas = moedas + num_moedas_ganho WHERE id = id_pc;
-    DELETE FROM inimigo WHERE id = id_inimigo;
+  IF (NEW.id_pc = NEW.id_vencedor) THEN
+    SELECT xp_ganho, num_moedas_ganho INTO p_xp_ganho, p_num_moedas_ganho 
+    FROM inimigo WHERE id = NEW.id_inimigo;
+
+    UPDATE pc 
+    SET xp = xp + p_xp_ganho, 
+    moedas = moedas + p_num_moedas_ganho 
+    WHERE id = NEW.id_pc;
+  END IF;
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER inimigo_derrotado_trigger
 AFTER INSERT ON combate
 FOR EACH ROW
-EXECUTE PROCEDURE inimigo_derrotado(:NEW.id_pc, :NEW.id_inimigo);
+EXECUTE PROCEDURE inimigo_derrotado();
